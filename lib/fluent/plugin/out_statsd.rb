408994c1d223a698f428d5fb16592980058dcead
James Ward <james@notjam.es>
2018-12-12 07:31:29 -0500

    add sampling rate configuration option and param

--- lib/fluent/plugin/out_statsd.rb
+++ lib/fluent/plugin/out_statsd.rb
@@ -6,6 +6,7 @@ module Fluent
     Fluent::Plugin.register_output('statsd', self)
 
     config_param :flush_interval, :time, :default => 1
+    config_param :sample_rate, :float, :default => 1
     config_param :host, :string, :default => 'localhost'
     config_param :port, :string, :default => '8125'
 
@@ -34,20 +35,22 @@ module Fluent
 
     def write(chunk)
       chunk.msgpack_each {|record|
+        record['statsd_rate'] = sample_rate unless record.key? 'statsd_rate'
+
         if statsd_type = record['statsd_type']
           case statsd_type
           when 'timing'
-            @statsd.timing record['statsd_key'], record['statsd_timing'].to_f
+            @statsd.timing record['statsd_key'], record['statsd_timing'].to_f, sample_rate: record['statsd_rate']
           when 'gauge'
-            @statsd.gauge record['statsd_key'], record['statsd_gauge'].to_f
+            @statsd.gauge record['statsd_key'], record['statsd_gauge'].to_f, sample_rate: record['statsd_rate']
           when 'count'
-            @statsd.count record['statsd_key'], record['statsd_count'].to_f
+            @statsd.count record['statsd_key'], record['statsd_count'].to_f, sample_rate: record['statsd_rate']
           when 'set'
-            @statsd.set record['statsd_key'], record['statsd_set']
+            @statsd.set record['statsd_key'], record['statsd_set'], sample_rate: record['statsd_rate']
           when 'increment'
-            @statsd.increment record['statsd_key']
+            @statsd.increment record['statsd_key'], sample_rate: record['statsd_rate']
           when 'decrement'
-            @statsd.decrement record['statsd_key']
+            @statsd.decrement record['statsd_key'], sample_rate: record['statsd_rate']
           end
         end
       }
